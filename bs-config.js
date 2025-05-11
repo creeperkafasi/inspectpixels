fe1df748ab1de4a4cf629aef1afd0db8754bcda0
Deniz Tunç <49831545+creeperkafasi@users.noreply.github.com>
2025-05-12 00:12:19 +0300

    Simple proof of concept

---
+++ bs-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    server: true,
+    files: ["**/*.html", "**/*.css", "**/*.js"],
+    open: true, // Don't automatically open browser
+    notify: false // Disable the "Connected to BrowserSync" notification
+};
